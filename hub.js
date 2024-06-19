5f21088df6a3f51d6ad13cf958de50065b5efb4b
HaZeShade <123483567+HaZeShade@users.noreply.github.com>
2024-06-18 22:45:09 -0500

    Add files via upload

---
+++ hub.js
@@ -0,0 +1,9 @@
+shenanigansOriginalButton = document.getElementById('shenanigans-original-button')
+
+function onShenanigansOriginalClick() {
+    window.location.replace(window.location + "shenanigans-original/");
+}
+
+
+
+shenanigansOriginalButton.onclick = onShenanigansOriginalClick()
