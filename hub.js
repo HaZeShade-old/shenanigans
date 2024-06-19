419bc7320ab8dc906023a548841aa2de6b3ce07c
HaZeShade <123483567+HaZeShade@users.noreply.github.com>
2024-06-18 23:42:32 -0500

    Update hub.js

--- hub.js
+++ hub.js
@@ -1,6 +1,18 @@
+shenanigansRemasteredButton = document.getElementById('shenanigans-remastered-button')
 shenanigansOriginalButton = document.getElementById('shenanigans-original-button')
+ciLogsButton = document.getElementById('ci-logs-button')
+
+shenanigansRemasteredButton.onclick = function(e) {
+    e.preventDefault();
+    location.href = 'https://hazeshade.github.io/shenanigans/shenanigans-remastered/';
+}
 
 shenanigansOriginalButton.onclick = function(e) {
     e.preventDefault();
     location.href = 'https://hazeshade.github.io/shenanigans/shenanigans-original/';
+}
+
+ciLogsButton.onclick = function(e) {
+    e.preventDefault();
+    location.href = 'https://hazeshade.github.io/shenanigans/ci-logs/';
 }
