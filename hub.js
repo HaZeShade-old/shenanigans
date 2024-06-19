84af8bf8b3c6427bf30ff1b9258745e9e55caa4f
HaZeShade <123483567+HaZeShade@users.noreply.github.com>
2024-06-18 23:25:40 -0500

    Update hub.js

--- hub.js
+++ hub.js
@@ -2,8 +2,5 @@ shenanigansOriginalButton = document.getElementById('shenanigans-original-button
 
 shenanigansOriginalButton.onclick = function(e) {
     e.preventDefault();
-
-    // Replace localhost and the folder name
-    // based on your setup
     location.href = 'https://hazeshade.github.io/shenanigans/shenanigans-original/';
 }
