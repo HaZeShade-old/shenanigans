39d77c3e1242123bf64f8ad8b13dc9d11413dca9
HaZeShade <123483567+HaZeShade@users.noreply.github.com>
2024-06-19 00:58:19 -0500

    Update menu.js

--- shenanigans-original/menu.js
+++ shenanigans-original/menu.js
@@ -1,7 +1,9 @@
-ch1Button = document.getElementById('button-ch1')
+document.addEventListener('DOMContentLoaded', function() {
+    const ch1Button = document.getElementById('button-ch1');
 
-ch1Button.onclick = function(e) {
-    e.preventDefault();
-    console.log(location.href)
-    location.href = location.href + 'chapters/1.html';
-}
+    ch1Button.onclick = function(e) {
+        e.preventDefault();
+        console.log(location.href);
+        location.href = location.href + 'chapters/1.html';
+    };
+});
